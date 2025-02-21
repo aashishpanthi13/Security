1fa83083ed65fb26b6604c9e6ce4256328c88e34
Aashish <aashishpanthi853@gmail.com>
2025-02-21 17:16:50 +0545

    Jwt Commit

--- src/main/java/com/example/springsecurity/Config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/Config/SecurityConfig.java
@@ -18,18 +18,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Autowired
     UserDetailsService userDetailsService;
+    @Autowired
+    private JwtFilter jwtFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception  {
         http.csrf(customizer->customizer.disable());
         http.authorizeHttpRequests(request-> request.requestMatchers("/register","/login").permitAll().anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 //    @Bean
